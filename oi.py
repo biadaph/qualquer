8ecba4ce32a7d402d513bea80740ba0efb77f596
biadaph <167120391+biadaph@users.noreply.github.com>
2024-04-22 15:20:07 -0300

    Create oi.py

---
+++ oi.py
@@ -0,0 +1,94 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Try programiz.pro")
+x = 1.0
+
+fy = 2 * (x ** 4) - 2 * (x ** 2) - x + 3
+gy = 5 * x / (3 ** x) - 1.1*(x ** x)
+ha = 2 * (x ** 2)
+hb = x / 2
+hc = 1
+hd = x + 1 / 2
+
+print (fy)
+print (gy)
+
+if x <= 0:
+    print (ha)
+elif 0 < x < 1:
+    print (hb)
+elif x == 1:
+    print (hc)
+else :
+    print (hd)
+    
+def f(h):
+    return 2 * (h ** 4) - 2 * (h ** 2) - h + 3
+
+fy1 = f(1)
+print(fy1)
+
+def g(h):
+    return 5 * h / (3 ** h) - 1.1*(h ** h) 
+    
+fy2 = g(1)
+print(fy2)
+
+def t(h):
+    return 2 * (h ** 2)
+    
+fy3 = t(1)
+
+def p(h):
+    return h / 2
+fy4 = p(1)
+
+def u(h):
+    return 1
+fy5 = u(1)
+
+def z(h):
+    return  h + 1 / 2
+fy6 = z (1)
+
+if x <= 0:
+    print (fy3)
+elif 0 < x < 1:
+    print (fy4)
+elif x == 1:
+    print (fy5)
+else :
+    print (fy6)
+    
+def y(u):
+    print (u)
+    return 5
+y(8)
+def J(u):
+    return u * y(u)
+
+print(J(8))]
+
+def k (y):
+    return 
+
+
+    
+
+
+
+
+
+
+
+
+    
+    
+def oi (x):
+    x  = x**2
+# print (x)
+
+def kkk (oi):
+    pass
+
+
